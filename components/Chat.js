74b0f1fee4a0382860b51dea12ca82bc563d2d6d
Renata C R <152975781+crrenatacr@users.noreply.github.com>
2024-11-14 13:56:08 +0100

    onSendUseEffectCorrections

--- components/Chat.js
+++ components/Chat.js
@@ -7,7 +7,7 @@ import CustomActions from './CustomActions';
 import MapView from 'react-native-maps';
 
 
-const Chat = ({ route, db, navigation, isConnected, storage }) => {
+const Chat = ({ route, db, isConnected, storage }) => {
   const [messages, setMessages] = useState([]);
   const collectionName = "messages";
 
@@ -17,24 +17,23 @@ const Chat = ({ route, db, navigation, isConnected, storage }) => {
   
 
   const onSend = (newMessages) => {
-    const message = newMessages[0];
-    console.log("User data:", { userID, name });
+    const message = newMessages[0] || newMessages;
     if (userID && name) {
-      addDoc(collection(db, collectionName), {
-        _id: message._id || Math.floor(Math.random() * 1000000), // Generate ID if it doesnt exist
-        text: message.text,
-        createdAt: new Date(),
-        user: {
-          _id: userID, // Uses userID from route.params
-          name: name, // Uses name from route.params
-          avatar: message.user.avatar || null,
-        },
-        system: message.system || false,
-      });
+        addDoc(collection(db, collectionName), {
+            _id: message?._id || Math.floor(Math.random() * 1000000), // Generate ID if it doesnt exist
+            ...message,
+            createdAt: new Date(),
+            user: {
+                _id: userID, // Uses userID from route.params
+                name: name, // Uses name from route.params
+                avatar: message?.user?.avatar || null,
+            },
+            system: message?.system || false,
+        });
     } else {
-      console.error("Incomplete User Data: ", { userID, name });
+        console.error("Incomplete User Data: ", { userID, name });
     }
-  };
+};
 
   const renderBubble = (props) => {
     return (
@@ -83,48 +82,32 @@ const Chat = ({ route, db, navigation, isConnected, storage }) => {
   }
 
   useEffect(() => {
-    navigation.setOptions({ title: name });
-  }, [name, navigation]);
-
-  let unsubMessages;
-  useEffect(() => {
     if (isConnected === true) {
-      const q = query(collection(db, collectionName), orderBy("createdAt", "desc"));
-      unsubMessages = onSnapshot(q, (querySnapshot) => {
-        const messagesFirestore = querySnapshot.docs.map(doc => {
-          const data = doc.data();
-          return {
-            _id: data._id,
-            text: data.text,
-            createdAt: data.createdAt.toDate(), // Convert TimeStamp para Date
-            user: {
-              _id: data.user._id,
-              name: data.user.name,
-              avatar: data.user.avatar || null,
-              image: data.image || null,
-            },
-            system: data.system || false,
-          };
+        const q = query(
+            collection(db, collectionName),
+            orderBy("createdAt", "desc")
+        );
+        unsubMessages = onSnapshot(q, (querySnapshot) => {
+            const newMessages = [];
+            querySnapshot.docs.map((doc) => {
+                newMessages.push({
+                    _id: doc.id,
+                    ...doc.data(),
+                    createdAt: new Date(doc.data().createdAt.toMillis()),
+                });
+            });
+            setMessages(newMessages);
         });
-        setMessages(messagesFirestore);
-      });
     } else {
-      loadCachedMessages();
+        loadCachedMessages();
     }
 
     // Clean up code
     return () => {
-      if (unsubMessages) unsubMessages();
+        if (unsubMessages) unsubMessages();
     };
-  }, [isConnected, db]);
+}, [isConnected, db]);
 
-  const cacheMessages = async (messagesToCache) => {
-    try {
-      await AsyncStorage.setItem("messages", JSON.stringify(messagesToCache));
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
   //Function called when isConnected prop is false in useEffect()
   const loadCachedMessages = async () => {
     const cachedMessages = (await AsyncStorage.getItem("messages")) || [];
